ce9435adf7c6be4f4534fe1358925f683ee361a3
jviejo <jviejo@JVIEJO>
2022-09-01 13:02:50 +0200

    otro

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.use(express.static('public'))
 app.get("/api", async (req, res)=>{
     res.send(new Date())
 })
+app.get("/", async (req, res)=>{
+    res.send(new Date())
+})
 
-app.listen(4444)
+const port = process.env.PORT || 8080
+app.listen(port)
